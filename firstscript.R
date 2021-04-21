f266230c28b13f88ad1c6b50d3fa2ce9110325f0
pd3389 <pd3389@pjwstk.edu.plgit config --global user.name PD3389>
2021-04-21 22:27:10 +0200

    modify firstproject

--- firstscript.R
+++ firstscript.R
@@ -1,6 +1,7 @@
 install.packages("httr")
 install.packages("jsonlite")
 
+
 library(httr)
 require(jsonlite)
 
@@ -13,3 +14,23 @@ weatherDF<-as.data.frame(weatherJson)
 weatherDF
 
 weatherText
+
+x<- 124.5
+x
+class(x)
+is.vector(x)
+
+x<-"123"
+x
+is.vector(x)
+
+x<-c(1,2,4,5,6,7,8,9,10)
+x
+y<-c(2,2)
+class(x)
+
+as.integer(x)
+wynik<-x+y
+
+wynik<-x*y
+wynik
